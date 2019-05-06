4db5cc427304bdb64c2b273ddf18e459ee41ad1c
fehead <im.fehead@gmail.com>
2019-05-06 22:07:42 +0900

    자바(JAVA) 리듬게임 만들기 강좌 14강
    박자에 맞추어 노트 찍기 (How To Make Java Rhythm Game #14)
    https://youtu.be/NZxMsEKdjbM

---
+++ DynamicKIUP/src/main/java/dynamic_beat_14/Main.java
@@ -0,0 +1,19 @@
+package dynamic_beat_14;
+
+public class Main {
+	public static final int	SCREEN_WIDTH = 1280;
+	public static final int	SCREEN_HEIGHT = 720;
+	
+	/* NOTE_SPEED = 7
+	 * SLEEP_TIME = 10 인경우.
+	 * 1초에 700(7 * 1000(ms) / 10)만큼 노트가 이동한다. 
+	 */
+	public static final int	NOTE_SPEED = 3; // 한번에 노트가 7픽셀씩 이동 
+	public static final int	SLEEP_TIME = 10; // SLEEP_TIME ms 에 한번씩 쉰다(1초에 1000/SLEEP_TIME)
+	public static final int REACH_TIME = 2;
+	
+	public static void main(String[] args) {
+		new DynamicBeat();
+	}
+
+}
